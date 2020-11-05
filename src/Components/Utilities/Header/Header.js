5f03ab93398cd7441ac96d5f37174710155fd4ee
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-11-05 15:25:06 +0000

    Style header

--- src/Components/Utilities/Header/Header.js
+++ src/Components/Utilities/Header/Header.js
@@ -4,11 +4,17 @@ import logo from './header-logo.png'
 export default function Header({children}) {
   return (
     <div style={{background:'#fff', padding: '15px'}} className="fixed-top text-center justify-content-center">
-      <a href="/">
-        <img src={ logo } alt="Bolt Protect" style={{ maxHeight: "40px" }} className="headerlogo d-flex img-fluid" />
-      </a>
-      <div>
-        {children}
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-6">
+            <a href="/">
+              <img src={ logo } alt="Bolt Protect" style={{ maxHeight: "40px" }} className="headerlogo d-flex img-fluid" />
+            </a>
+          </div>
+          <div className="col-6">
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   )
