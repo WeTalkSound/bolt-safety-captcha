d9d49c7c9e3f7035f58c919371a842e58515460c
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-11-25 09:35:10 +0000

    Push

--- src/Components/Pages/Home/Leaderboard.js
+++ src/Components/Pages/Home/Leaderboard.js
@@ -11,7 +11,7 @@ export default class Leaderboard extends Component {
 
     componentDidMount() {
         console.log("Showing Scores")
-        fetch(`https://bolt-campaigns.firebaseio.com/bolt-safety-captcha/high-scores.json?orderBy=%22country%22&endAt=%22${this.props.country}%22&limitToLast=${this.props.limit}`, {
+        fetch(`https://us-central1-bolt-campaigns.cloudfunctions.net/boltCaptchaFilterHighScores?limit=${this.props.limit}&country=${this.props.country}`, {
             method: 'GET'
         })
             .then(res => res.json())
@@ -22,7 +22,7 @@ export default class Leaderboard extends Component {
                 this.setState({ scores, error: "" })
             })
             .catch(error => {
-              fetch(`https://us-central1-bolt-campaigns.cloudfunctions.net/boltCaptchaFilterHighScores?limit=${this.props.limit}&country=${this.props.country}`, {
+              fetch(`https://us-central1-bolt-campaigns.cloudfunctions.net/boltCaptchaAlsoFilterHighScores?limit=${this.props.limit}&country=${this.props.country}`, {
                   method: 'GET'
               })
                   .then(res => res.json())
@@ -33,7 +33,7 @@ export default class Leaderboard extends Component {
                       this.setState({ scores, error: "" })
                   })
                   .catch(error => {
-                    fetch(`https://us-central1-bolt-campaigns.cloudfunctions.net/boltCaptchaAlsoFilterHighScores?limit=${this.props.limit}&country=${this.props.country}`, {
+                    fetch(`https://bolt-campaigns.firebaseio.com/bolt-safety-captcha/high-scores.json?orderBy=%22country%22&endAt=%22${this.props.country}%22&limitToLast=${this.props.limit}`, {
                         method: 'GET'
                     })
                         .then(res => res.json())
